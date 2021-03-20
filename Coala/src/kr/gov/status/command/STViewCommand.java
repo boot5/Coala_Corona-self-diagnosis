50bdaf8919b4f7b47e87ca4421751f386ce2bef4
kang <chanel748596@gmail.com>
2021-03-20 15:11:27 +0900

    test

--- Coala/src/kr/gov/status/command/STViewCommand.java
+++
@@ -1,9 +0,0 @@
-package kr.gov.status.command;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface STVoewCommand {
-	
-	public void excute(HttpServletRequest request, HttpServletResponse response);
-}
--- Coala/src/kr/gov/status/model/StatusDTO.java
+++
@@ -1,5 +0,0 @@
-package kr.gov.status.model;
-
-public class StatusDTO {
-
-}
